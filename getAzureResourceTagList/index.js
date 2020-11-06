8e8013da488aa0ced06f7772fd0fb17e6520e8a3
Ryen Tang <kiazhi@hotmail.com>
2020-11-06 11:46:23 +0800

    draft

---
+++ getAzureResourceTagList/index.js
@@ -0,0 +1,27 @@
+const msRestAzure = require('@azure/ms-rest-nodeauth');
+const azResources = require('@azure/arm-resources');
+
+const azureAppId        = process.env["AzureApplicationId"] ;
+const azureSecret       = process.env["AzureSecret"] ;
+const azureTenant       = process.env["AzureTenant"] ;
+const azureSubscription = process.env["AzureSubscription"] ;
+const azureRegion       = process.env["AzureRegion"] ;
+
+module.exports = async function (context, req) {
+
+    const credential = await msRestAzure.loginWithServicePrincipalSecret(
+        azureAppId,
+        azureSecret,
+        azureTenant
+    ) ;
+
+    const client = await new azResources.ResourceManagementClientContext(
+        credential,
+        azureSubscription
+    ) ;
+
+    const resourcesTagsList = await new azResources.TagsOperations(client).list() ;
+
+    context.res.json(resourcesTagsList) ;
+
+} ;
