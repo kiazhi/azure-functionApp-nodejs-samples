0a342c93eb22ed3192d527f4902a71dd96f10b18
Ryen Tang <kiazhi@hotmail.com>
2020-11-17 12:14:57 +0800

    test

---
+++ getAzureServiceIssueEventList/index.js
@@ -0,0 +1,40 @@
+const msRestAzure = require('@azure/ms-rest-nodeauth');
+const https = require('https');
+const axios = require('axios');
+
+const azureAppId        = process.env["AzureApplicationId"] ;
+const azureSecret       = process.env["AzureSecret"] ;
+const azureTenant       = process.env["AzureTenant"] ;
+const azureSubscription = process.env["AzureSubscription"] ;
+const azureRegion       = process.env["AzureRegion"] ;
+
+module.exports = async function (context, req) {
+
+    const credential = await msRestAzure.loginWithServicePrincipalSecret(
+        azureAppId,
+        azureSecret,
+        azureTenant
+    ) ;
+
+    const token = await (await credential.getToken()).accessToken;
+
+    let url = `https://management.azure.com/subscriptions/${azureSubscription}/providers/Microsoft.ResourceHealth/events?api-version=2018-07-01` ;
+
+    const agent = new https.Agent(
+        {
+            rejectUnauthorized: false,
+        }
+    );
+
+    const res = await axios.get(url,
+        {
+            headers: {
+                httpsAgent: agent,
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    ) ;
+
+    context.res.json(res.data.value) ;
+
+} ;
